ce8910151688191a213177841b6c83d68c64e87a
Zachary Moss <zacharymoss@Zacharys-MacBook-Pro.local>
2018-07-25 15:46:58 -0500

    Added erorrs

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import axios from 'axios';
-import fire from './fire.png';
 import './App.css';
 
 // need to do ajax request to server to send lat and lon inputs
@@ -8,12 +7,8 @@ import './App.css';
 // then on completion server needs to send api data back to front-end
 // then front-end needs to display that info
 
-
-
 class App extends Component {
   state = {
-    lat: undefined,
-    lon: undefined,
     temp: undefined,
     mean: undefined,
     median: undefined,
@@ -26,6 +21,13 @@ class App extends Component {
   onSubmit = (e) => {
     e.preventDefault();
 
+    this.setState(() => ({ city: undefined }));
+    this.setState(() => ({ country: undefined }));
+    this.setState(() => ({ error: undefined }));
+    this.setState(() => ({ mean: undefined }));
+    this.setState(() => ({ median: undefined }));
+    this.setState(() => ({ mode: undefined }));
+
     let lat = e.target.elements.lat.value;
     let lon = e.target.elements.lon.value;
 
@@ -34,47 +36,41 @@ class App extends Component {
       "lon": lon
     }
 
-    this.setState(() => ({ lat: undefined }));
-    this.setState(() => ({ lon: undefined }));
-    this.setState(() => ({ city: undefined }));
-    this.setState(() => ({ country: undefined }));
-    this.setState(() => ({ temp: undefined }));
-    this.setState(() => ({ error: undefined }));
-    this.setState(() => ({ mean: undefined }));
-    this.setState(() => ({ median: undefined }));
-    this.setState(() => ({ mode: undefined }));
-
-
     // axios post request
     const self = this;
     axios.post('/axios', data)
     .then(function(response){
-      
-      let mean = response.data.mean;
-      //console.log("returned mean: " + response.data.mean);
-      let median = response.data.median;
-      //console.log("returned median: " + response.data.median);
-      let mode = (response.data.mode);
-      //console.log("returned mode: " + mode);
-      
-      mean = (1.8 * (mean - 273) + 32).toFixed(2);
-      median = (1.8 * (median - 273) + 32).toFixed(2);
-      mode = (1.8 * (mode - 273) + 32).toFixed(2);
-      self.setState(() => ({ mean: mean }));
-      self.setState(() => ({ median: median }));
-      self.setState(() => ({ mode: mode }));      
 
-      
+      console.log('mean response: ' + response.data.mean);
+      let mean = response.data.mean;
+      let median = response.data.median;
+      let mode = response.data.mode;
+
+      if (mean) {
+        mean = (1.8 * (mean - 273) + 32).toFixed(2);
+        median = (1.8 * (median - 273) + 32).toFixed(2);
+        mode = (1.8 * (mode - 273) + 32).toFixed(2);
+        self.setState(() => ({ mean: mean }));
+        self.setState(() => ({ median: median }));
+        self.setState(() => ({ mode: mode }));  
+      }    
+
       let city = response.data.city;
       self.setState(() => ({ city: city }));
       let country = response.data.country;
       self.setState(() => ({ country: country }));
       
+      console.log("error returned: " + response.data.error);
     })
-    .catch(function(error){
-      console.log(error);
+    .catch(function(err){
+      console.log("error: " + err.response);
     });
 
+    if (!lat || !lon) {
+      this.setState(() => ({ error: "Please enter both coordinates." }));
+    } else {
+      this.setState(() => ({ error: undefined }));
+    }
   }
 
   render() {
@@ -85,29 +81,30 @@ class App extends Component {
             Weather App
           </h1>
         </header>
-        <div className="app-body">
-          <p><i>Enter latitude and longitude for forecasted temperatures.</i></p>
-          <form onSubmit={this.onSubmit}>
-            <div className="row">
-              <div className="input-container">
-                <p>Latitude</p>
-                <input name="lat"></input>
+        <div className="row">
+          <div className="app-body">
+            <p><i>Enter latitude and longitude for forecasted temperatures.</i></p>
+            <form onSubmit={this.onSubmit}>
+              <div className="row">
+                <div className="input-container">
+                  <p>Latitude</p>
+                  <input name="lat"></input>
+                </div>
+                <div className="input-container">
+                  <p>Longitude</p>
+                  <input name="lon"></input>
+                </div>
+                <button>Check</button>
               </div>
-              <div className="input-container">
-                <p>Longitude</p>
-                <input name="lon"></input>
-              </div>
-              <button>Check</button>
-            </div>
-          </form>
+            </form>
 
-          {this.state.error && <p>{this.state.error}</p>}
-          {this.state.city && <p className="city"><strong>{this.state.city}, {this.state.country}</strong></p>}
-          {this.state.temp && <p>Local Temp: {this.state.temp}</p>}
-          {this.state.mean && <p><span className="forecast">16 Day Forecast</span></p>}
-          {this.state.mean && <p>Mean: {this.state.mean} &#8457;</p>}
-          {this.state.median && <p>Median: {this.state.median} &#8457;</p>}
-          {this.state.mode && <p>Mode: {this.state.mode} &#8457;</p>}
+            {this.state.error && <p className="error"><i>{this.state.error}</i></p>}
+            {this.state.city && <p className="city"><strong>{this.state.city}, {this.state.country}</strong></p>}
+            {this.state.mean && <p><span className="forecast">16 Day Forecast</span></p>}
+            {this.state.mean && <p>Mean: {this.state.mean} &#8457;</p>}
+            {this.state.median && <p>Median: {this.state.median} &#8457;</p>}
+            {this.state.mode && <p>Mode: {this.state.mode} &#8457;</p>}
+          </div>
         </div>
       </div>
     );
--- server.js
+++ server.js
@@ -8,7 +8,6 @@ const functions = require('./functions.js');
 const app = express();
 
 
-
 app.use(cors());
 
 app.use(express.static(path.join(__dirname, 'front-end/build')));
@@ -22,27 +21,19 @@ app.listen(port, function() {
     console.log("app server listening on" + port);
 });
 
-
-
-
 // API call
 app.post('/axios', (req, res) => {
     res.set('Content-Type', 'text/json');
     const lat = req.body.lat;
     const lon = req.body.lon;
-    console.log("lat: " + lat);
-    console.log("lon: " + lon);
 
     const url = 'https://api.openweathermap.org/data/2.5/forecast?lat=' + lat + '&lon=' + lon + '&cnt=16&appid=b3da3c9942f24bac56e8898c51e03757';
-
-    console.log("url: "+ url);
     
     axios.get(url).then(json => {
         let arrLength = json.data.list.length;
         let tempArray = [];
         
         let city = json.data.city.name;
-        console.log("city: " + city);
         let country = json.data.city.country;      
         
         for (i = 0; i < arrLength; i++) {
@@ -51,27 +42,19 @@ app.post('/axios', (req, res) => {
             tempArray.push(x);
         };
         
-
         // functions
         let mean = functions.meanFunc(tempArray);
-        console.log("mean: " + mean);
         let median = functions.medianFunc(tempArray);
-        console.log("median in kelvin: " + median);
         let n = [];
         n = functions.modeFunc(tempArray);
-        n = n[0];
-        console.log("mode in kelvin: " + n);
-        console.log(tempArray);
-        
+        n = n[0];        
         
         res.send({ mean: mean, median: median, mode: [n], city: city, country: country });
 
     }).catch(error => {
-    console.log("this is the error: " + error.response);
-    res.send({ error: error.response });
+    console.log("this is the error: " + error);
+    res.send({ error: "no response" });
     });
-    
 });
-
 
 module.exports = app;
